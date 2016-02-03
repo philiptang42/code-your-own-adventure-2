f1b590ce2b34a88e636688c0b5f927a223fe3dcf
philiptang42 <philiptang42@gmail.com>
2016-02-03 16:35:14 -0500

    embedded if statements within switch

--- code_your_own_adventure_2.js
+++ code_your_own_adventure_2.js
@@ -1,14 +1,28 @@
 var user = prompt("What's your name?").toUpperCase();
+var time = 'night'
+var day = 'weekend'
 
 switch(user) {
   case 'NAME':
-    console.log("clever");
+    if(time === 'morning' || day === 'weekend') {
+      console.log("clever");
+    } else {
+      console.log("ok");
+    }
     break;
   case 'PHILIP':
-    console.log("No, that's me");
+    if(time === 'night' && day === 'weekend') {
+      console.log("No, that's me");
+    } else {
+      console.log("You are an impostor");
+    }
     break;
   case 'DRAGON':
-    console.log("That's not a name");
+    if(time === 'morning' && day === 'weekday') {
+      console.log("That's not a name");
+    } else {
+      console.log("Nope.");
+    }
     break;
   default:
     console.log("I don't think that's your name");
