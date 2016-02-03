59e798b15ee3e316c638f904bd0aec264c1cd34d
philiptang42 <philiptang42@gmail.com>
2016-02-03 16:10:30 -0500

    switch statement

--- code_your_own_adventure_2.js
+++ code_your_own_adventure_2.js
@@ -1,1 +1,15 @@
 var user = prompt("What's your name?").toUpperCase();
+
+switch(user) {
+  case 'NAME':
+    console.log("clever");
+    break;
+  case 'PHILIP':
+    console.log("No, that's me");
+    break;
+  case 'DRAGON':
+    console.log("That's not a name");
+    break;
+  default:
+    console.log("I don't think that's your name");
+};
